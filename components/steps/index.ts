21ca401edcb5bc4b033ad9da97bdafc9dde2a3d7
Alexey <2317727+napstor@users.noreply.github.com>
2026-04-28 21:49:19 +0300

    Add Drill step component

--- components/steps/index.ts
+++ components/steps/index.ts
@@ -1,3 +1,4 @@
 export { CompositionStep } from "./CompositionStep";
+export { DrillStep } from "./DrillStep";
 export { TheoryStep } from "./TheoryStep";
 export { VocabularyStep } from "./VocabularyStep";
