9a6ae9e3a21559de8b2b939de5a69410ad94c8f7
Alexey <2317727+napstor@users.noreply.github.com>
2026-04-28 22:30:23 +0300

    Add dashboard atomic widgets

---
+++ components/dashboard/index.ts
@@ -0,0 +1,4 @@
+export { ConsistencyCalendar } from "./ConsistencyCalendar";
+export type { ConsistencyDay } from "./ConsistencyCalendar";
+export { MasteryRing } from "./MasteryRing";
+export { StreakDisplay } from "./StreakDisplay";
