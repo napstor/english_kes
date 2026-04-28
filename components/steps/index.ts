90c7ec14be538fbbe4fa5af72755cf50efc21c23
Alexey <2317727+napstor@users.noreply.github.com>
2026-04-28 22:11:51 +0300

    Add Speaking step component

--- components/steps/index.ts
+++ components/steps/index.ts
@@ -1,5 +1,6 @@
 export { CompositionStep } from "./CompositionStep";
 export { DrillStep } from "./DrillStep";
+export { SpeakingStep } from "./SpeakingStep";
 export { TheoryStep } from "./TheoryStep";
 export { TranslateStep } from "./TranslateStep";
 export { VocabularyStep } from "./VocabularyStep";
