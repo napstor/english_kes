2862c48555b3b1d70d00a6b596652bf14943e943
Alexey <2317727+napstor@users.noreply.github.com>
2026-04-28 22:09:49 +0300

    Add Translate step component

--- components/steps/index.ts
+++ components/steps/index.ts
@@ -1,4 +1,5 @@
 export { CompositionStep } from "./CompositionStep";
 export { DrillStep } from "./DrillStep";
 export { TheoryStep } from "./TheoryStep";
+export { TranslateStep } from "./TranslateStep";
 export { VocabularyStep } from "./VocabularyStep";
